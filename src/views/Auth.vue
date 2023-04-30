ac8cea0240abd0c4ec0e0fccd0b797f689b72594
RafSchapendonk <rafschapendonk@outlook.com>
2023-04-30 02:01:58 +0200

    Working on token request

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -5,11 +5,54 @@
 </template>
 
 <script lang="ts">
+import axios from "axios";
+import qs from "qs";
+import { Buffer } from "buffer";
+// window.Buffer = Buffer;
+
 import Spinner from "../components/Spinner.vue";
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const auth_token = Buffer.from(
+  `${client_id}:${client_secret}`,
+  "utf-8"
+).toString("base64");
 
 export default {
   components: {
     Spinner,
+  },
+
+  mounted() {
+    this.getToken();
+  },
+  methods: {
+    async getToken() {
+      let data = qs.stringify({
+        'grant_type': 'client_credentials'
+      });
+
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          // TO-DO Fix hardcoded auth token.
+          'Authorization': 'Basic MzI5ODk5NDU3OTJiNDNjOTg1MjY0OTkzZDBjOTZjNmM6NmQwOWRkODA5YTVlNDI1MTkxN2Q1MWIzZTE4NjEwYzc=',
+        },
+        data: data
+      };
+
+      axios.request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // process: "process/browser",
+      // stream: "stream-browserify",
+      // zlib: "browserify-zlib",
+      // util: "util",
+      web3: path.resolve(__dirname, "./node_modules/web3/dist/web3.min.js"),
+    },
+  },
   define: {
     'process.env': {}
   }
